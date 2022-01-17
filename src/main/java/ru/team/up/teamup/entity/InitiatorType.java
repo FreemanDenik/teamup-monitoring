3a0b284d9a1b824f5fc194f1b5708a0108753563
Stanislav Ivashchenko <isn-88@ya.ru>
2022-01-17 13:40:59 +0700

    bugfix/TEAMUP-86 Правки замечаний по мониторингу

--- src/main/java/ru/team/up/teamup/entity/InitiatorType.java
+++ src/main/java/ru/team/up/teamup/entity/InitiatorType.java
@@ -1,5 +1,13 @@
 package ru.team.up.teamup.entity;
 
+
+/**
+ * Кто именно создал событие для Кафки:
+ * SYSTEM - система
+ * USER - пользователь
+ * MANAGER - менеджер
+ * ADMIN - администратор
+ */
 public enum InitiatorType {
     SYSTEM, USER, MANAGER, ADMIN
 }
--- src/main/java/ru/team/up/teamup/entity/Status.java
+++ src/main/java/ru/team/up/teamup/entity/Status.java
@@ -1,5 +1,11 @@
 package ru.team.up.teamup.entity;
 
+
+/**
+ * Статус события для Кафки:
+ * SUCCESS - успешно
+ * FAILURE - неуспешно
+ */
 public enum Status {
     SUCCESS, FAILURE
 }
