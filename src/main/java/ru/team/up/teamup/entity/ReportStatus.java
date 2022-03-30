b43bf24e33c55c2651d357a2a64afe1b3ee6f6da
Yura Katkov <94790420+JZawR@users.noreply.github.com>
2022-03-30 12:07:17 +0300

    feature/TEAMUP-23 поменял status на reportStatus, добавил поле reportName

--- src/main/java/ru/team/up/teamup/entity/ReportStatus.java
+++ src/main/java/ru/team/up/teamup/entity/ReportStatus.java
@@ -1,11 +1,10 @@
 package ru.team.up.teamup.entity;
 
-
 /**
  * Статус события для Кафки:
  * SUCCESS - успешно
  * FAILURE - неуспешно
  */
-public enum Status {
+public enum ReportStatus {
     SUCCESS, FAILURE
 }
