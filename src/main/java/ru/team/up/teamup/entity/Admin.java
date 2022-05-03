0a9a7712a270c6df18784efb56b260a5efd90b92
dmitrykoryanov <dmitry.koryanov@yandex.ru>
2022-05-03 12:09:48 +0300

    feature/TEAMUP-32 Необходимо создать или поправить тесты в репозитории супа и мониторинга * В сущности Report и Admin добавлена аннотация @Builder для удобства работы с данными сущностями * Добавлены два теста репозиториев AdminRepository и DataRepository

--- src/main/java/ru/team/up/teamup/entity/Admin.java
+++ src/main/java/ru/team/up/teamup/entity/Admin.java
@@ -1,6 +1,7 @@
 package ru.team.up.teamup.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 
+@Builder
 @Data
 @Document
 @NoArgsConstructor
